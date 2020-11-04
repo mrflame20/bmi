4822ddb50b91ccd826b29e0e5817028d2807b1c5
kpseudo <karangadani@gmail.com>
2020-11-04 14:13:10 +0530

    New update

---
+++ lib/Controller/controller_binding.dart
@@ -0,0 +1,10 @@
+import 'package:BMI/Controller/bmi_controller.dart';
+import 'package:get/get.dart';
+
+class ControllerBinding extends Bindings{
+  @override
+  void dependencies() {
+    Get.lazyPut(() => BmiController());
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
+import 'package:BMI/Controller/controller_binding.dart';
 import 'package:flutter/material.dart';
 import 'package:BMI/Homepage.dart';
+import 'package:get/get.dart';
 
 void main() => runApp(BMICalculator());
 
 class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       theme: ThemeData.dark(),
+      initialBinding: ControllerBinding(),
       home: HomePage(),
     );
   }
